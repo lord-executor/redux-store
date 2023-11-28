efd499f235d312f0c047a015271bd2dd83d0d266
Lukas Angerer <executor.dev@gmail.com>
2023-11-28 19:27:59 +0100

    Listing books and managing collections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { selectBookCollection, selectBooks } from './books.selectors';
+import { Observable } from 'rxjs';
+import { Book } from './books.model';
+import { GoogleBooksService } from './books.service';
+import { Store } from '@ngrx/store';
+import { BooksActions, BooksApiActions } from './books.actions';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,30 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'redux-store';
+  public books: Observable<readonly Book[]>;
+  public bookCollection: Observable<Book[]>;
+ 
+  addBook(bookId: string) {
+    this.store.dispatch(BooksActions.addBook({ bookId }));
+  }
+ 
+  removeBook(bookId: string) {
+    this.store.dispatch(BooksActions.removeBook({ bookId }));
+  }
+ 
+  constructor(
+    private readonly booksService: GoogleBooksService,
+    private readonly store: Store
+  ) {
+    this.books = this.store.select(selectBooks);
+    this.bookCollection = this.store.select(selectBookCollection);
+  }
+ 
+  ngOnInit() {
+    this.booksService
+      .getBooks()
+      .subscribe((books) =>
+        this.store.dispatch(BooksApiActions.retrievedBookList({ books }))
+      );
+  }
 }
